638b45f450190aaf34e398c91923fcef852fd819
Ryubexxx <christopherlet213@gmail.com>
2026-07-10 16:49:59 +0800

    Added the NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,35 @@
+import { AiOutlineHome } from "react-icons/ai";
+import '../styles/Navbar.css'
 
 export default function Navbar() {
     return (
-        <div className="navBar">
+        <nav className="navBar">
             <div className="navBar-left">
-                <h2>Pixel</h2>
-                <h2>Parts</h2>
+                <h2 className='pixel'>Pixel</h2>
+                <h2 className='parts'>Parts</h2>
             </div>
 
             <div className="navBar-right">
-                <li>
-                    
-                </li>
+                <ul>
+                    <li>
+                        <a href="" className='nav-link'></a>
+                        <AiOutlineHome />
+                        Home
+                    </li>
+
+                    <li>
+                        Products
+                    </li>
+
+                    <li>
+                        Cart
+                    </li>
+
+                    <li>
+                        Login
+                    </li>
+                </ul>
             </div>
-        </div>
+        </nav>
     )
 } 
