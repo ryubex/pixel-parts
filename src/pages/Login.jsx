c705e4d11cd7c55a0d5959f8066983b7f6a2135b
Ryubexxx <christopherlet213@gmail.com>
2026-07-13 20:11:20 +0800

    Made the sign in page. Currently done at sign in user login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,47 @@
+import '../styles/Login.css'
+
 import { Link } from "react-router";
 
 const Login = () => {
   return (
     <>
-      <p>THIS IS LOGIN PAGE</p>
+      <div className="signin">
+        <div className='signin-header'>
+          <h3>Sign in to PixelParts</h3>
+        </div>
+
+        <div className='signin-body'>
+          <form>
+            <div className='signin-username'>
+              <label>
+                Username or email address
+              </label>
+
+              <input type="text" />
+            </div>
+
+            <div className='signin-password'>
+              <label>
+                Password
+              </label>
+
+              <input type="text" />
+            </div>
+
+            <div>
+              <input type="submit" value="Sign in" className='signin-btn'/>
+            </div>
+          </form>
+
+          <div className='webInput-login'>
+
+          </div>
+        </div>
+
+        <div className='signin-footer'>
+          <p>This is the footer</p>
+        </div>
+      </div>
     </>
   );
 };
