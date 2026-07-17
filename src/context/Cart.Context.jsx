578c35987293c0113a6d62640c74f89f97741514
Ryubexxx <christopherlet213@gmail.com>
2026-07-17 16:25:21 +0800

    Added increase and decrease quantity button for items in cart page

--- src/context/Cart.Context.jsx
+++ src/context/Cart.Context.jsx
@@ -5,8 +5,62 @@ export const CartContext = createContext()
 const CartProvider = ({children}) => {
     const [cart, setCart] = useState([])
 
+    function addToCart(product) {
+        setCart((prevCart) => {
+            const existingItem = prevCart.find(
+                (item) => item.id === product.id
+            )
+
+            if (existingItem) {
+                return prevCart.map((item) =>
+                    item.id === product.id
+                    ? {...item, quantity: item.quantity + 1}
+                    : item
+                )
+            }
+
+            return [...prevCart, {...product, quantity: 1}]
+        })
+    }
+
+    function increaseQuantity(id){
+        setCart((prevCart) => 
+            prevCart.map((item) =>
+                item.id === id
+                    ?{
+                     ...item,
+                     quantity: item.quantity + 1,   
+                    }
+                : item
+            )
+        )
+    }
+
+    function decreaseQuantity(id){
+        setCart((prevCart) =>
+            prevCart
+                .map((item) =>
+                    item.id === id
+                        ?{
+                            ...item,
+                            quantity: item.quantity - 1
+                        }
+                    : item
+                )
+            .filter((item) => item.quantity > 0)    
+        )
+    }
+
+
     return (
-        <CartContext.Provider value={{cart, setCart}}>
+        <CartContext.Provider 
+            value={{
+                cart, 
+                addToCart,
+                increaseQuantity,
+                decreaseQuantity
+            }}
+            >
             {children}
         </CartContext.Provider>
     )
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,43 @@
 import { Link } from "react-router";
 
+import { useContext } from "react";
+import { CartContext } from "../context/Cart.Context";
+
+
 const Cart = () => {
+  const {cart, increaseQuantity, decreaseQuantity} = useContext(CartContext)
+
   return (
-    <>
-      <p>THIS IS CART PAGE</p>
-    </>
+    <div>
+      <h1>Your Cart</h1>
+
+      {cart.map((item) => (
+        <div key={item.id}>
+          <img
+            src={item.image}
+            alt={item.title}
+            width="80"
+          />
+
+          <h3>{item.title}</h3>
+
+          <p>${item.price}</p>
+
+          <p>Quatity: {item.quantity}</p>
+
+          <button onClick={() => increaseQuantity(item.id)}>
+            +
+          </button>
+
+          <span>{item.quantity}</span>
+
+          <button onClick={() => decreaseQuantity(item.id)}>
+            -
+          </button>
+
+        </div>
+      ))}
+    </div>
   );
 };
 
