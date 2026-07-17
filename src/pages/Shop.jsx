17e7f08a4adb4f76535c911ea8a836e448a92f0c
Ryubexxx <christopherlet213@gmail.com>
2026-07-17 15:28:32 +0800

    connected the API and made the products/shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,52 @@
 import { Link } from "react-router";
+import ProductCard from "../components/ProductCard"
+import '../styles/Shop.css'
+
+import { useEffect, useState } from "react";
 
 const Shop = () => {
+
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/category/electronics")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch products")
+        }
+
+        return res.json()
+      })
+      
+      .then((data) => {
+        setProducts(data)
+      })
+
+      .catch((err) => {
+        setError(err.message)
+      })
+
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
   return (
     <>
-      <p>THIS IS SHOP PAGE</p>
+      <div className="shop-container">
+        {
+          products.map(product => (
+            <ProductCard
+              key={product.id}
+              product={product}
+            />
+          ))
+        }
+      </div>
     </>
-  );
-};
+  )
+}
 
 export default Shop;
