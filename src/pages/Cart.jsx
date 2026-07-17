272ac0e54a4d6daefb047c75c4c7999cc0cd3177
Ryubexxx <christopherlet213@gmail.com>
2026-07-17 16:29:40 +0800

    Added total price for items in cart and checkout button

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,6 +6,10 @@ import { CartContext } from "../context/Cart.Context";
 
 const Cart = () => {
   const {cart, increaseQuantity, decreaseQuantity} = useContext(CartContext)
+  const total = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  )
 
   return (
     <div>
@@ -37,6 +41,10 @@ const Cart = () => {
 
         </div>
       ))}
+
+      <h2>Total: ${total.toFixed(2)}</h2>
+
+          <button>Checkout</button>
     </div>
   );
 };
